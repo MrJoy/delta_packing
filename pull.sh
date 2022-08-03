ea67dbf1bdbf21669a81988deb490cf462ff87cb
Jon Frisby <jfrisby@mrjoy.com>
2022-08-03 16:12:52 -0700

    Fetch/process versions oldest-to-newest.
    This makes it safe to run `delta.sh` while `pull.sh` is running.  Without this,

--- pull.sh
+++ pull.sh
@@ -4,7 +4,7 @@ IFS=$'\n\t'
 
 # N.B. Not handling pagination here, and having such a high limit is quite slow.
 echo "Fetching list of versions..."
-curl --silent 'https://en.wikipedia.org/w/index.php?title=Transformers&limit=5000&action=history' | perl -pse 's/^.*?oldid=(\d+).*$/\1/gm' | grep -v -E '[<":> \s]' | grep -E '[[:digit:]]' | uniq | while read VERSION; do
+curl --silent 'https://en.wikipedia.org/w/index.php?title=Transformers&limit=5000&action=history' | perl -pse 's/^.*?oldid=(\d+).*$/\1/gm' | grep -v -E '[<":> \s]' | grep -E '[[:digit:]]' | uniq | sort -n | while read VERSION; do
   if [ -e "raw/$VERSION" ]; then
     continue
   fi
