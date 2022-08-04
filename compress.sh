c9f272f5036331a8db3d7fea6e41b7ea018dacca
Jon Frisby <jfrisby@mrjoy.com>
2022-08-03 17:21:33 -0700

    Improve compression settings, and update test results.

--- compress.sh
+++ compress.sh
@@ -2,34 +2,42 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-# Compression results for an early sample set comprising 52 versions of the Transformers article,
-# downloaded as just wikitext are as follows:
+# Compression results for 3,715 versions of the Transformers article, downloaded as just wikitext
+# are as follows:
 #
 # For the delta chain (initial ver + bsdiff snapshots):
-# -rw-r--r--   1 jonathonfrisby  staff   114176 Aug  3 14:57 delta.tar
-# -rw-r--r--   1 jonathonfrisby  staff    33062 Aug  3 15:20 delta.tar.bz2
-# -rw-r--r--   1 jonathonfrisby  staff    38144 Aug  3 15:20 delta.tar.lz4
-# -rw-r--r--   1 jonathonfrisby  staff    31850 Aug  3 15:20 delta.tar.lzip
-# -rw-r--r--   1 jonathonfrisby  staff    32779 Aug  3 15:20 delta.tar.pzstd
-# -rw-r--r--   1 jonathonfrisby  staff    31916 Aug  3 15:20 delta.tar.xz
+# -rw-r--r--   1 jonathonfrisby  staff    5111808 Aug  3 16:29 delta.tar
+# -rw-r--r--   1 jonathonfrisby  staff    1706877 Aug  3 16:33 delta.tar.bz2
+# -rw-r--r--   1 jonathonfrisby  staff    1883513 Aug  3 16:33 delta.tar.lz4
+# -rw-r--r--   1 jonathonfrisby  staff    1571222 Aug  3 16:33 delta.tar.lzip
+# -rw-r--r--   1 jonathonfrisby  staff    1582158 Aug  3 16:33 delta.tar.pzstd
+# -rw-r--r--   1 jonathonfrisby  staff    1571420 Aug  3 16:33 delta.tar.xz
 #
 # For the raw data:
-# -rw-r--r--   1 jonathonfrisby  staff  3183104 Aug  3 14:57 raw.tar
-# -rw-r--r--   1 jonathonfrisby  staff   155332 Aug  3 15:20 raw.tar.bz2
-# -rw-r--r--   1 jonathonfrisby  staff   737694 Aug  3 15:20 raw.tar.lz4
-# -rw-r--r--   1 jonathonfrisby  staff    24739 Aug  3 15:20 raw.tar.lzip
-# -rw-r--r--   1 jonathonfrisby  staff    25691 Aug  3 15:20 raw.tar.pzstd
-# -rw-r--r--   1 jonathonfrisby  staff    24824 Aug  3 15:20 raw.tar.xz
+# -rw-r--r--   1 jonathonfrisby  staff  154850816 Aug  3 16:29 raw.tar
+# -rw-r--r--   1 jonathonfrisby  staff    9512410 Aug  3 16:34 raw.tar.bz2
+# -rw-r--r--   1 jonathonfrisby  staff   19959772 Aug  3 16:34 raw.tar.lz4
+# -rw-r--r--   1 jonathonfrisby  staff     348775 Aug  3 16:35 raw.tar.lzip
+# -rw-r--r--   1 jonathonfrisby  staff     311237 Aug  3 16:34 raw.tar.pzstd
+# -rw-r--r--   1 jonathonfrisby  staff     313196 Aug  3 16:34 raw.tar.xz
 #
-# In both cases, lzip produces the best result.  As such, we're sticking with that for the time
-# being.  I'm recording the options used for each compression type here for posterity and ease of
-# re-evaluation later.
-for ARCHIVE in $(cd out; ls *.tar); do
-  echo "Compressing out/${ARCHIVE}"
-  # cat "$ARCHIVE" | xz -9 --extreme --threads=0 > "${ARCHIVE}.xz"
-  # cat "$ARCHIVE" | lz4 --best --sparse > "${ARCHIVE}.lz4"
-  # cat "$ARCHIVE" | bzip2 -9 > "${ARCHIVE}.bz2"
-  # cat "$ARCHIVE" | pzstd --ultra -22 --force --stdout > "${ARCHIVE}.pzstd"
+# Early testing with a small number of versions (52) had lzip as the unambiguous winner in all
+# cases.  With a significantly larger dataset, ztsd and xz both beat lzip on the raw dataset by
+# about 10% -- but not on the delta chain dataset.
+# for ARCHIVE in $(cd out; ls *.tar); do
+#   echo "Compressing out/${ARCHIVE}"
 
-  cat "out/$ARCHIVE" | lzip -9 --match-length=273 > "out/${ARCHIVE}.lzip"
-done
+#   cat "out/$ARCHIVE" | xz -9 --extreme --threads=0 > "out/${ARCHIVE}.xz"
+#   cat "out/$ARCHIVE" | lz4 --best --sparse > "out/${ARCHIVE}.lz4"
+#   cat "out/$ARCHIVE" | bzip2 -9 > "out/${ARCHIVE}.bz2"
+#   cat "out/$ARCHIVE" | lzip -9 --match-length=273 > "out/${ARCHIVE}.lzip"
+#   cat "out/$ARCHIVE" | zstd --ultra -22 --long=31 --force --stdout > "out/${ARCHIVE}.zstd_0"
+#   cat "out/$ARCHIVE" | zstd --ultra -22 --long=31 --compress-literals --target-compressed-block-size=131072 --force --stdout > "out/${ARCHIVE}.zstd_1"
+#   cat "out/$ARCHIVE" | zstd --ultra -22 --long=31 --no-compress-literals --stream-size=$(wc -c "out/$ARCHIVE") --force --stdout > "out/${ARCHIVE}.zstd_2"
+# done
+
+echo "Compressing out/delta.tar"
+cat "out/delta.tar" | lzip -9 --match-length=273 > "out/delta.tar.lzip"
+
+echo "Compressing out/raw.tar"
+cat "out/raw.tar" | zstd --ultra -22 --long=31 --no-compress-literals --stream-size=$(wc -c "out/raw.tar") --force --stdout > "out/raw.tar.zstd"
