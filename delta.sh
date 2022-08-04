03339267c37ba5eb546be066b48db1c14a9362bc
Jon Frisby <jfrisby@mrjoy.com>
2022-08-03 18:24:56 -0700

    Use a better mechanism for picking a delta source.

--- delta.sh
+++ delta.sh
@@ -6,27 +6,35 @@ set +e
 INITIAL=$(ls delta | grep -v bsdiff)
 set -e
 
-if [ ! -z "$INITIAL" ]; then
-  echo "Removing previous initial version, $INITIAL"
-  rm "delta/$INITIAL"
+if [ $(ls delta | wc -l) -gt 0 ]; then
+  echo "Clearing old delta chain"
+  rm delta/*
 fi
 
 for VERSION in $(cd raw; ls | sort -n); do
   PREVIOUS=$(cd raw; ls | sort -n | grep -B 1 "$VERSION" | head -1)
 
   if [ "$PREVIOUS" -eq "$VERSION" ]; then
-    if [ -e "delta/$VERSION" ]; then
-      continue
-    fi
     echo "Copying initial version ${VERSION}"
     cp "raw/$VERSION" delta/
     continue
   fi
 
-  if [ -e "delta/${VERSION}.bsdiff" ]; then
-    continue
+  echo "Computing diff candidates for ${VERSION}"
+  if [ $(ls candidates | wc -l) -gt 0 ]; then
+    rm candidates/*
   fi
 
-  echo "Computing diff for ${PREVIOUS}..${VERSION}"
-  bsdiff "raw/$PREVIOUS" "raw/$VERSION" "delta/${VERSION}.bsdiff"
+  for CANDIDATE_VERSION in $(cd raw; ls | sort -n | grep -B 4 "$VERSION"); do
+    if [ "$CANDIDATE_VERSION" -eq "$VERSION" ]; then
+      cp "raw/$VERSION" candidates/
+      continue
+    fi
+
+    bsdiff "raw/$CANDIDATE_VERSION" "raw/$VERSION" "candidates/${CANDIDATE_VERSION}_${VERSION}.bsdiff"
+  done
+
+  echo "Copying best candidate to delta/"
+  CHOSEN_CANDIDATE=$(wc -c candidates/* | grep -v total | sort -n | head -1 | awk '{ print $2 }' | cut -d/ -f2)
+  cp "candidates/$CHOSEN_CANDIDATE" delta/
 done
