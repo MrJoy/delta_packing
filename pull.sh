04b9eaa2d4c96297117554c5159d3f84028a21e9
Jon Frisby <jfrisby@mrjoy.com>
2022-08-07 19:20:03 -0700

    Parameterize this a bit.

--- pull.sh
+++ pull.sh
@@ -2,12 +2,17 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+ARTICLE="Transformers"
+COUNT=5000
+# ARTICLE="Recession"
+# COUNT=5000
+
 # N.B. Not handling pagination here, and having such a high limit is quite slow.
 echo "Fetching list of versions..."
-curl --silent 'https://en.wikipedia.org/w/index.php?title=Transformers&limit=5000&action=history' | perl -pse 's/^.*?oldid=(\d+).*$/\1/gm' | grep -v -E '[<":> \s]' | grep -E '[[:digit:]]' | uniq | sort -n | while read VERSION; do
+curl --silent "https://en.wikipedia.org/w/index.php?title=${ARTICLE}&limit=${COUNT}&action=history" | perl -pse 's/^.*?oldid=(\d+).*$/\1/gm' | grep -v -E '[<":> \s]' | grep -E '[[:digit:]]' | uniq | sort -n | while read VERSION; do
   if [ -e "raw/$VERSION" ]; then
     continue
   fi
   echo "Fetching version $VERSION"
-  curl --silent "https://en.wikipedia.org/w/index.php?title=Transformers&action=raw&oldid=${VERSION}" > "raw/$VERSION"
+  curl --silent "https://en.wikipedia.org/w/index.php?title=${ARTICLE}&action=raw&oldid=${VERSION}" > "raw/$VERSION"
 done
